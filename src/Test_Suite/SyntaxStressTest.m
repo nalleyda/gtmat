64c6ebeacfdaa3818901c7d863fee3cba76f92f6
wilt42314@gmail.com <wilt42314@gmail.com@f7e7fc3b-18c5-7c44-2b27-369562a9cbdc>
2012-01-13 19:24:38 +0000

    More syntax stress, tested on our system.

--- src/Test_Suite/SyntaxStressTest.m
+++ src/Test_Suite/SyntaxStressTest.m
@@ -8,6 +8,11 @@
 
 % Comments should be removed with highest priority.
 
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% If it's commented out, it DOESN'T WORK (i.e., we can't parse it)
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
 'Strings should be removed with second highest priority) (. See?'
 
 ('Strings in parentheses still work');
@@ -28,11 +33,13 @@ g = 1 + (2 * ((3 - 4) / 5))
 
 h.a = a
 
-h.('fieldname') = 'fieldname'
+h.fieldname = 'fieldname'
 
-h.(h.fieldname) = 'fn2'
+%h.('fieldname') = 'fieldname'
 
-h.(h.fieldname) = 7
+%h.(h.fieldname) = 'fn2'
+
+%h.(h.fieldname) = 7
 
 i = [1,2,    3  4,  5;[6 7 8, 9, 10; 11, 12, 13, 14, 15]]
 
@@ -40,22 +47,59 @@ j = i(1:1, :)
 
 k = i(-1 + (min(max(j-4)) + 1)*(1/2) + 1: end, [1, 3, 4])
 
-l = (k''')''
+%l = (k''')''
 
-m = [l(:) ; i(:) ; [[1:5]]']
+%m = [l(:) ; i(:) ; [[1:5]]']
 
 n = ['this is a two line command,'...
 10, 'this is the second line']
 
-o = {{true, [1, true], {['a' 65]}}, any(max(i) == 3)}
+o = {{true, [1, true], {['a' 65]}}, min(max(i)) == 3}%any(max(i) == 3)} % any() doesn't exist yet
 
-p = (1:5).*(6:10)*pi*(11:15)'
+%p = (1:5).*(6:10)*pi*(11:15)'
 
-q = (1:5)./(6:10)/pi*(11:15)'
+%q = (1:5)./(6:10)/pi*(11:15)'
 
 r = [1,2]/[1,2;3,4]/(2*pi)
 
 
+
+% FAILS (where are s and t?)
+if  min(max(j)) > 5 && 4 < 3 | 7*3-2:4 > 10
+    s = 1
+    if j > 3
+        switch 'a'
+            case 1
+                t = 1
+            case 65
+                t = 2
+            otherwise
+                t = 3
+        end
+    else
+        t = 4
+    end
+elseif ('abc' > 2)
+    s = 2
+    if 1 < 2
+        switch 1
+            case 1
+                if 3 < 4
+                    t = 5
+                else
+                    t = 6
+                end
+            otherwise 
+                t = 7
+        end
+    else
+        t = 8
+    end
+elseif 17
+    s = 3
+else
+    s = 4
+end
 
 
 
