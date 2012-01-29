415975839ef97c39f2df8d50ee52291ea69847bc
wilt42314@gmail.com <wilt42314@gmail.com@f7e7fc3b-18c5-7c44-2b27-369562a9cbdc>
2012-01-29 03:45:33 +0000

    Surprise! More dummy classes.

---
+++ trunk/src/functions/Negate.java
@@ -0,0 +1,11 @@
+package functions;
+
+import jmatrix.*;
+
+public class Negate {//e.g., -x, rather than a boolean negation ~x
+	
+	public static MatObject negate(MatObject o) throws Exception{
+		throw new Exception("Calling a dummy method.");
+	}
+
+}
