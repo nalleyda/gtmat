8ad11bb781b7132023b046153e4ed7efaaebb24e
wilt42314@gmail.com <wilt42314@gmail.com@f7e7fc3b-18c5-7c44-2b27-369562a9cbdc>
2012-01-29 03:31:34 +0000

    More dummies

---
+++ trunk/src/functions/LessEqual.java
@@ -0,0 +1,9 @@
+package functions;
+
+import jmatrix.*;
+
+public class LessEqual {
+	public static MatObject lessEqual(MatObject o1, MatObject o2) throws Exception{
+		throw new Exception("Calling a dummy method.");
+	}
+}
