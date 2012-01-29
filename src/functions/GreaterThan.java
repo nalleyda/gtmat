608b705faa72c45c9f749485c9d173daf7536249
wilt42314@gmail.com <wilt42314@gmail.com@f7e7fc3b-18c5-7c44-2b27-369562a9cbdc>
2012-01-29 03:31:34 +0000

    More dummies

---
+++ src/functions/GreaterThan.java
@@ -0,0 +1,11 @@
+package functions;
+
+import jmatrix.*;
+
+public class GreaterThan {
+	
+	public static MatObject greaterThan(MatObject o1, MatObject o2) throws Exception{
+		throw new Exception("Calling a dummy method.");
+	}
+
+}
