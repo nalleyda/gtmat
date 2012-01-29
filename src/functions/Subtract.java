e5c3b39c4853d791fc7218bf53a836aa8c39210f
wilt42314@gmail.com <wilt42314@gmail.com@f7e7fc3b-18c5-7c44-2b27-369562a9cbdc>
2012-01-29 03:22:27 +0000

    Dummy methods for functions package - preparing for live migration with new architecture.

---
+++ src/functions/Subtract.java
@@ -0,0 +1,9 @@
+package functions;
+
+import jmatrix.*;
+
+public class Subtract {
+	public static MatObject subtract(MatObject o1, MatObject o2) throws Exception{
+		throw new Exception("Calling a dummy method.");
+	}
+}
