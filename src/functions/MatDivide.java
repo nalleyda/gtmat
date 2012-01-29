825d88380a170e9626694b7e773ebd94c183487f
wilt42314@gmail.com <wilt42314@gmail.com@f7e7fc3b-18c5-7c44-2b27-369562a9cbdc>
2012-01-29 03:24:55 +0000

    Another dummy class

---
+++ src/functions/MatDivide.java
@@ -0,0 +1,9 @@
+package functions;
+
+import jmatrix.*;
+
+public class MatDivide {
+	public static MatObject matDivide(MatObject o1, MatObject o2) throws Exception{
+		throw new Exception("Calling a dummy method.");
+	}
+}
