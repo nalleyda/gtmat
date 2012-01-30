3b38b2cccf08b459fb32788ca7242bff1000114a
wilt42314@gmail.com <wilt42314@gmail.com@f7e7fc3b-18c5-7c44-2b27-369562a9cbdc>
2012-01-30 01:56:12 +0000

    Yet more dummy classes.

---
+++ trunk/src/functions/HorizontalConcatenate.java
@@ -0,0 +1,12 @@
+package functions;
+
+import jmatrix.*;
+
+public class HorizontalConcatenate {
+	public static MatObject horizontalConcatenate(MatObject... arr) throws Exception{
+		throw new Exception("Calling a dummy method.");
+	}
+	public static MatObject horizontalConcatenateCell(MatObject... arr) throws Exception{
+		throw new Exception("Calling a dummy method.");
+	}
+}
---
+++ trunk/src/functions/VerticalConcatenate.java
@@ -0,0 +1,13 @@
+package functions;
+
+import jmatrix.*;
+
+public class VerticalConcatenate {
+	public static MatObject verticalConcatenate(MatObject... arr) throws Exception{
+		throw new Exception("Calling a dummy method.");
+	}
+	
+	public static MatObject verticalConcatenateCell(MatObject... arr) throws Exception{
+		throw new Exception("Calling a dummy method.");
+	}
+}
