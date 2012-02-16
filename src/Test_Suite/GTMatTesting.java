24dc709fec8c2a75bd1c2df53b42f1a1426232cf
sean.pohjalainen@gmail.com <sean.pohjalainen@gmail.com@f7e7fc3b-18c5-7c44-2b27-369562a9cbdc>
2012-02-16 08:09:27 +0000

    I've tried a lot of ways to get the variables from the two different workspaces into one place, and this is the only successful one so far. Much more irritating than expected...

--- src/Test_Suite/GTMatTesting.java
+++ src/Test_Suite/GTMatTesting.java
@@ -2,11 +2,14 @@ package Test_Suite;
 
 import parser.*;
 import main.Main;
+import workspace.Variable;
+import jmatrix.*;
 import matlabcontrol.MatlabConnectionException;
 import matlabcontrol.MatlabInvocationException;
 import matlabcontrol.MatlabProxy;
 import matlabcontrol.MatlabProxyFactory;
 import javax.swing.DefaultListModel;
+import java.util.HashMap;
 
 public class GTMatTesting {
 	public static String[] chs = {"", "", "ch2", "ch3", "ch4", "ch5", "ch6", "ch7", "ch8", "ch9", "ch10", "ch11", "ch12", "ch13", "ch14", "ch15", "ch16", "ch17"};
@@ -14,12 +17,14 @@ public class GTMatTesting {
 	public static String dirA = "listings/";
 	public static String dirB = "Validation/chapterScripts/";
 	public static int endCh;
+	public static int beginCh;
 	public static DefaultListModel gtMatVars;
 	
-	public static void testGTMat(int stopCh){
+	public static void testGTMat(int startCh, int stopCh){
 		long start;
 		endCh = stopCh;
-		for (int i = 2; i <= endCh; i++){
+		beginCh = startCh;
+		for (int i = beginCh; i <= endCh; i++){
 			start = System.currentTimeMillis();
 			//proxy.eval(chs[i]);
 			
@@ -32,12 +37,16 @@ public class GTMatTesting {
 			//System.out.println((System.currentTimeMillis()-start) +"should be >4000");
 			//System.out.println(path);
 			System.out.println("========="+path+"============");
+			
 			GTParser.process(path);
 		}
-		
+		//gtMatVars is a deep list of all vars in a list of Variables
+		long s = System.currentTimeMillis();
+		while(System.currentTimeMillis()-s<2000){
+			
+		}
 		gtMatVars = Main.wstack.peek().getVarList();
-		
-		
+		testMatlab(beginCh, endCh);
 		
 	}
 	public static void testCh(int chNum){
@@ -50,14 +59,15 @@ public class GTMatTesting {
 		
 	}
 	
-	public static void testMatlab(int endCh){
-		//int endCh = 2;
+	public static void testMatlab(int beginCh, int endCh){
+		//Issues: dealing with structs, cells, graphing audio
+		//Determining whether they are equal in a nice manner
 		try{
 			MatlabProxyFactory factory = new MatlabProxyFactory();
 			MatlabProxy proxy = factory.getProxy();
 			
 			
-				for (int i = 2; i <= endCh; i++){
+				for (int i = beginCh; i <= endCh; i++){
 					String path = dirB + chs[i]+".m";
 					try{
 						proxy.eval(chs[i]);
@@ -65,11 +75,80 @@ public class GTMatTesting {
 					}catch(MatlabInvocationException mie){
 						System.out.println("Matlab invocation exception.");
 					}
+					//proxy.eval("")
 				}
 				
+				if (gtMatVars!=null){
+					System.out.println("gtMatVars!=null, entering if block to write vars to matlab");
+								//option 1. write workspace var values to .txt w/ save, and 
+								//write their names to another txt file with dlmwrite and who/s
+								//option 2. try to bring the variables of gtmat workspace into matlab workspace
+								// using proxy.setVariable(var + "_", var.getValue()) or something
+	//					proxy.eval("a = whos");
+	//					proxy.eval("dlmwrite('matlabTest.txt', a)");
+						HashMap<String, Boolean> results = new HashMap<String, Boolean>(gtMatVars.size());
+						for (int i = 0; i<gtMatVars.size(); i++){
+							Variable v = (Variable)gtMatVars.get(i);
+							//String s = (String)v.getData().toString();
+							System.out.println(v.getVarName()+" is of type "+MatObject.getClass(v.getData()));
+							System.out.println(v.getVarName()+".toString() => /"+v.getData().toString()+"/");
+							try{
+								// works for int, double, and 1d arrays:proxy.setVariable(v.getVarName()+"_", Double.parseDouble(v.getData().toString()));
+								MatObject d = v.getData();
+								if (d instanceof Matrix){
+									Matrix dm = (Matrix)d;
+									proxy.setVariable(v.getVarName()+"_", dm.data);
+									//proxy.eval("disp("+v.getVarName()+"-"+v.getVarName()+"_");
+//									proxy.setVariable("diff", 0);
+//									
+//									proxy.eval("diff = check("+v.getVarName()+", "+v.getVarName()+"_)");
+//									Object r = proxy.getVariable("diff");
+//									double firstArg = ((double[])r)[0];
+//									if (firstArg > 0){
+//										results.put(v.getVarName(), true);
+//									}
+//									else{
+//										results.put(v.getVarName(), false);
+//									}
+//									proxy.eval("r = check("+v.getVarName()+", "+v.getVarName()+"_");
+//									Object[] o = proxy.returningEval("check", 1);
+								}
+								//proxy.setVariable(v.getVarName()+"_", 
+							}catch(MatlabInvocationException mie){
+								System.out.println("Matlab invocation exception. Setting variable"+ v.getVarName()+"failed");
+								System.out.println("Exception msg:"+mie.getMessage());
+								System.out.println("StackTrace:"+mie.getStackTrace());
+								//results.put(v.getVarName(), false);
+							}
+						}
+						System.out.println(results.toString());
+				}
 			proxy.disconnect();
 		} catch(MatlabConnectionException mce){
 			System.out.println("Could not connect to Matlab instance.");
 		}
+	}
+	
+	public static void main(String[] args){
+		int[] arr = {1, 2, 3};
+		double[] dArr = {1.0, 2.0, 3.0};
+		
+		try{
+			MatlabProxyFactory factory = new MatlabProxyFactory();
+			MatlabProxy proxy = factory.getProxy();
+			try{
+				proxy.setVariable("intArray", arr);
+				proxy.setVariable("doubleArray", dArr);
+		
+			}catch(MatlabInvocationException mie){
+				//System.out.println("Matlab invocation exception. Setting variable"+ v.getVarName()+"failed");
+				System.out.println("Exception msg:"+mie.getMessage());
+				System.out.println("StackTrace:"+mie.getStackTrace());
+			}
+			proxy.disconnect();
+		} catch(MatlabConnectionException mce){
+			System.out.println("Could not connect to Matlab instance.");
+		}
+			
 	}
 }
