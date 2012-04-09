15671e9970dc5d95a21354bcfaa1570a17ae2b5a
joshua.sizemore@gmail.com <joshua.sizemore@gmail.com@f7e7fc3b-18c5-7c44-2b27-369562a9cbdc>
2012-04-09 03:54:19 +0000

    made undefined variable exception name the variable/function

--- trunk/src/gtmatException/UndefinedVariableException.java
+++ trunk/src/gtmatException/UndefinedVariableException.java
@@ -8,9 +8,15 @@ package gtmatException;
 
 public class UndefinedVariableException extends GTMatException {
 	
+	private String var;
+	
+	public UndefinedVariableException(String var) {
+		this.var=var;
+	}
+	
 	public String toString() {
 		message = "Error at " + filename + ": " + "line " + lineNumber + "\n" 
-					+ "Undefined function or variable";
+					+ "Undefined function or variable, " + var;
 		return message;
 	}
 }
