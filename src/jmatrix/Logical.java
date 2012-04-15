cace1758eec500e402028a7810cb429e27d88091
tahhope@gmail.com <tahhope@gmail.com@f7e7fc3b-18c5-7c44-2b27-369562a9cbdc>
2012-04-15 04:47:00 +0000

    Short circuit operators now working.

--- src/jmatrix/Logical.java
+++ src/jmatrix/Logical.java
@@ -2,7 +2,30 @@ package jmatrix;
 
 public class Logical extends MatObject{
 	
+	public boolean[] data;
+
+	public Logical(MatObject o1) {
+		data = new boolean[o1.n];
+		size = o1.size;
+		if(o1 instanceof Matrix) {
+			for(int i = 1; i <= o1.n; i++) {
+				data[i-1] = ((Matrix)o1).get(i) != 0;
+			}
+		} else if(o1 instanceof MatString) {
+			for(int i = 1; i <= o1.n; i++) {
+				data[i-1] = ((MatString)o1).get(i) != 0;
+			}
+		} else if(o1 instanceof Logical) {
+			for(int i = 1; i <= o1.n; i++) {
+				data[i-1] = ((Logical)o1).data[i];
+			}
+		}
+		
+	}
 	
+	public boolean get(int i) {
+		return data[i-1];
+	}
 
 	@Override
 	public MatObject copy() {
