a42d35694d21b2e15315ac6efef447a275a7a943
tahhope@gmail.com <tahhope@gmail.com@f7e7fc3b-18c5-7c44-2b27-369562a9cbdc>
2012-04-15 04:47:00 +0000

    Short circuit operators now working.

--- trunk/src/functions/ShortCircuitOr.java
+++ trunk/src/functions/ShortCircuitOr.java
@@ -4,6 +4,11 @@ import jmatrix.*;
 
 public class ShortCircuitOr {
 	public static MatObject shortCircuitOr(MatObject o1, MatObject o2) throws Exception{
-		throw new Exception("Calling a dummy method.");
+		Logical l1 = new Logical(o1);
+		Logical l2 = new Logical(o2);
+		Matrix ret = new Matrix(1);
+		ret.type = MatObject.Type.LOGICAL;
+		ret.set(1, l1.get(1) || l2.get(1) ? 1 : 0);
+		return ret;
 	}
 }
