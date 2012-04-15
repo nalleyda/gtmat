ac1f7f8db95ed86efd1f02767221a9fffee3121f
sean.pohjalainen@gmail.com <sean.pohjalainen@gmail.com@f7e7fc3b-18c5-7c44-2b27-369562a9cbdc>
2012-04-15 16:01:59 +0000

    boolNegate

--- src/functions/BoolNegate.java
+++ src/functions/BoolNegate.java
@@ -3,7 +3,23 @@ package functions;
 import jmatrix.*;
 
 public class BoolNegate {
+//	public static MatObject boolNegate(MatObject o) throws Exception{
+//		//throw new Exception("Calling a dummy method.");	
+//		return (MatObject)BoolNegate.class.getMethod("boolNegate", o.getClass()).invoke(null, new Object[] {o});
+//	}
+	
 	public static MatObject boolNegate(MatObject o) throws Exception{
-		throw new Exception("Calling a dummy method.");
+		Matrix out;
+		if ((o instanceof Matrix) || (o instanceof Logical)){
+			out = new Matrix(o);
+			Logical l1 = new Logical(o);
+			
+			for (int i = 1; i<=o.n; i++){
+				out.set(i, l1.get(i) == false ? 1 : 0);
+			}
+			
+			out.type = MatObject.Type.LOGICAL;
+		} else throw new Exception("Passed boolNegate(MatObject) an unexpected datatype. Expected inputs are Matrix and Logical.");
+		return out;
 	}
 }
