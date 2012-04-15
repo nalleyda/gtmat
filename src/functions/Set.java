603a8005e8a7f814638b20b32ab3dc7f35e0431f
joshua.sizemore@gmail.com <joshua.sizemore@gmail.com@f7e7fc3b-18c5-7c44-2b27-369562a9cbdc>
2012-04-15 05:16:08 +0000

    added set

---
+++ src/functions/Set.java
@@ -0,0 +1,124 @@
+package functions;
+
+import gtmatException.CustomException;
+import gtmatException.IndexOOBException;
+import gtmatException.MatrixDimensionsException;
+import gtmatException.TooFewInputsException;
+import gtmatException.TooManyInputsException;
+import jmatrix.MatObject;
+import jmatrix.Matrix;
+
+public class Set {
+	public static MatObject set(MatObject[] arr) throws Exception {
+		if(arr.length==4)
+			return (MatObject)Set.class.getMethod("set", arr[0].getClass(), arr[1].getClass(), arr[2].getClass(), arr[3].getClass()).invoke(null, new Object[] {arr[0], arr[1], arr[2], arr[3]});
+		if(arr.length==3)
+			return (MatObject)Set.class.getMethod("set", arr[0].getClass(), arr[1].getClass(), arr[2].getClass()).invoke(null, new Object[] {arr[0], arr[1], arr[2]});
+		if(arr.length>4)
+			throw(new TooManyInputsException());
+		else throw(new TooFewInputsException());
+	}
+	
+	
+	private static void set(Matrix m, Matrix rndx, Matrix cndx, Matrix vals) throws Exception {
+		if (rndx.size[MatObject.ROW] != 1
+				|| cndx.size[MatObject.ROW] != 1) {
+			throw(new CustomException("Matrix.set bad row or col vector"));
+		}
+		if (rndx.size[MatObject.COL] != vals.size[MatObject.ROW]
+				|| cndx.size[MatObject.COL] != vals.size[MatObject.COL]) {
+			throw(new CustomException("Matrix.set bad row or col vector size"));
+		}
+		int mxr = (int) ((Matrix) rndx.max().get(1, 1)).get(1);
+		int mxc = (int) ((Matrix) cndx.max().get(1, 2)).get(1);
+		if (mxr > m.size[MatObject.ROW]) {
+			MatObject[] inputs = {m, new Matrix(mxr), new Matrix(m.size[MatObject.COL])};
+			Extend.extend(inputs);
+		}
+		if (mxc > m.size[MatObject.COL]) {
+			MatObject[] inputs = {m, new Matrix(m.size[MatObject.ROW]), new Matrix(mxc)};
+			Extend.extend(inputs);
+		}
+		for (int ri = 1; ri <= rndx.size[MatObject.COL]; ri++) {
+			for (int ci = 1; ci <= cndx.size[MatObject.COL]; ci++) {
+				set(m, rndx.geti(ri), cndx.geti(ci),
+						vals.get(ri, ci));
+			}
+		}
+
+	}
+	
+	private static void set(Matrix m, double rndx, double cndx, double val) throws Exception {
+		MatObject[] inputs = {m, new Matrix(rndx), new Matrix(cndx), new Matrix(val)};
+		set(inputs);
+	}
+
+	/**
+	 * set a value in a matrix
+	 * @param r - the row
+	 * @param c - the column
+	 * @param val - the value
+	 * @throws Exception 
+	 */
+	private static void set(Matrix m, int r, int c, double val) throws Exception {
+		if (r > m.size[MatObject.ROW]) {
+			MatObject[] inputs = {m, new Matrix(r), new Matrix(m.size[MatObject.COL])};
+			Extend.extend(inputs);
+		}
+		if (c > m.size[MatObject.COL]) {
+			MatObject[] inputs = {m, new Matrix(m.size[MatObject.ROW]), new Matrix(c)};
+			Extend.extend(inputs);
+		}
+		m.data[(c - 1) * m.size[MatObject.ROW] + (r - 1)] = val;
+	}
+	
+	/**
+	 * set a value in a linearized array
+	 * @param i
+	 * @param val
+	 */
+	private static void set(Matrix m, int i, double val) {
+		if (i > m.n) {
+			throw(new CustomException("Matrix:set(i...) i too big"));
+		}
+		;
+		m.data[i - 1] = val;
+	}
+
+	/**
+	 * Set the values of matrix m at indices indices to values.
+	 * @param m the matrix to change
+	 * @param indices the indices to use 
+	 * @param values the values  to use
+	 */
+	private static void set(Matrix m, Matrix indices, Matrix values) {
+		int maxsize = m.size[MatObject.ROW] * m.size[MatObject.COL];
+		double[] ind = indices.data;
+		double[] vals = values.data;
+
+		if (ind.length != vals.length) {
+			throw(new MatrixDimensionsException());
+		}
+
+		for (int i = 0; i < ind.length; i++) {
+			if (ind[i] > maxsize) {
+				throw(new IndexOOBException());
+			}
+			m.data[i - 1] = vals[i];
+			throw(new IndexOOBException());
+		}
+	}
+
+	/**
+	 * Convenience method for set using Matrices.
+	 * @param m the matrix to change 
+	 * @param r the row index
+	 * @param c the column index
+	 * @param values the value
+	 * @throws Exception 
+	 */
+	private static void set(Matrix m, double[] r, double[] c, double value) throws Exception {
+		MatObject[] inputs = {m, new Matrix(r), new Matrix(c), new Matrix(value)};
+		set(inputs);
+	}
+}
