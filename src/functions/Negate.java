52ba0b6cf424beb61283572419a4372468e6223e
wilt42314@gmail.com <wilt42314@gmail.com@f7e7fc3b-18c5-7c44-2b27-369562a9cbdc>
2012-04-15 05:07:32 +0000

    Negate working

--- src/functions/Negate.java
+++ src/functions/Negate.java
@@ -5,7 +5,20 @@ import jmatrix.*;
 public class Negate {//e.g., -x, rather than a boolean negation ~x
 	
 	public static MatObject negate(MatObject o) throws Exception{
-		throw new Exception("Calling a dummy method.");
+		return (MatObject)Negate.class.getMethod("negate", o.getClass()).invoke(null, new Object[] {o});
+	}
+	
+	/**
+	 * horizontal concatenation (an array of matrices)
+	 * @param m
+	 * @return
+	 */
+	public static Matrix negate(Matrix m) {
+		Matrix res = new Matrix(m);
+		for (int i = 1; i <= res.n; i++) {
+			res.set(i, -m.get(i));
+		}
+		return res;
 	}
 
 }
